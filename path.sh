7d30fc17ce148187378fbf7e8fa0c4cfb565cc28
Drew Keller <drew@eightycolumns.com>
2017-09-17 19:45:03 -0400

    Add path.sh

---
+++ path.sh
@@ -0,0 +1,32 @@
+source "${HOME}/lib/util.sh"
+
+remove_from_path() {
+  (($# == 1)) || error 'remove_from_path() expects one argument'
+  local dir=$1
+
+  PATH=:${PATH}:
+  PATH=${PATH//:${dir}:/:}
+  PATH=$(echo "${PATH}" | sed -r 's/(^:|:$)//g')
+}
+
+export -f remove_from_path
+
+append_to_path() {
+  (($# == 1)) || error 'append_to_path() expects one argument'
+  local dir=$1
+
+  remove_from_path "${dir}"
+  PATH=${PATH}:${dir}
+}
+
+export -f append_to_path
+
+prepend_to_path() {
+  (($# == 1)) || error 'prepend_to_path() expects one argument'
+  local dir=$1
+
+  remove_from_path "${dir}"
+  PATH=${dir}:${PATH}
+}
+
+export -f prepend_to_path
